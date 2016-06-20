917cc1351904144fe786c4485680e1a212dae7bc
Sean Redmond <github-smr@sneakemail.com>
2016-06-20 12:45:50 -0400

    Fix eslint warnings
    Move styles definition before use. Add missing semicolons.

--- src/component.jsx
+++ src/component.jsx
@@ -1,35 +1,5 @@
 import React from 'react';
-import { render } from 'react-dom';
 import Radium from 'radium';
-
-const SkipNavigation = (props) => {
-  if (props.target) {
-    return (
-      <a id={props.id} href={'#' + props.target} style={styles.visuallyHidden}>
-      {props.linkText}
-      </a>
-    );
-  }
-
-  // If the target is not set, it is better to have no skip navigation link
-  // than a broken one, so we'll just output an empty span.
-  return (<span></span>);
-}
-
-
-SkipNavigation.propTypes = {
-  id: React.PropTypes.string,
-  lang: React.PropTypes.string,
-  linkText: React.PropTypes.string,
-  target: React.PropTypes.string.isRequired,
-};
-
-
-SkipNavigation.defaultProps = {
-  id: 'skip',
-  lang: 'en',
-  linkText: 'Skip to Main Content',
-};
 
 const styles = {
   visuallyHidden: {
@@ -45,6 +15,35 @@ const styles = {
       height: 'auto',
     },
   },
-}
+};
+
+const SkipNavigation = (props) => {
+  if (props.target) {
+    return (
+      <a id={props.id} href={'#' + props.target} style={styles.visuallyHidden}>
+      {props.linkText}
+      </a>
+    );
+  }
+
+  // If the target is not set, it is better to have no skip navigation link
+  // than a broken one, so we'll just output an empty span.
+  return (<span></span>);
+};
+
+
+SkipNavigation.propTypes = {
+  id: React.PropTypes.string,
+  lang: React.PropTypes.string,
+  linkText: React.PropTypes.string,
+  target: React.PropTypes.string.isRequired,
+};
+
+
+SkipNavigation.defaultProps = {
+  id: 'skip',
+  lang: 'en',
+  linkText: 'Skip to Main Content',
+};
 
 export default Radium(SkipNavigation);
