5228ab9cef7c9c1ccf2d29956381f4ef2adaa6ee
Rafael Hernandez <rafaelhernandez@nypl.org>
2016-08-15 12:23:44 -0400

    Removed Radium dependency and styles. Added basic styles to demo html page. Going forward all styles will come from consuming App

--- src/component.jsx
+++ src/component.jsx
@@ -1,26 +1,9 @@
 import React from 'react';
-import Radium from 'radium';
-
-const styles = {
-  visuallyHidden: {
-    position: 'absolute',
-    left: '-10000px',
-    top: 'auto',
-    width: '1px',
-    height: '1px',
-    overflow: 'hidden',
-    ':focus': {
-      left: 'auto',
-      width: 'auto',
-      height: 'auto',
-    },
-  },
-};
 
 const SkipNavigation = (props) => {
   if (props.target) {
     return (
-      <div id={props.id} style={styles.visuallyHidden} tabIndex="-1">
+      <div id={props.id} tabIndex="-1">
         <a href={`#${props.target}`} >
           {props.linkText}
         </a>
@@ -30,7 +13,6 @@ const SkipNavigation = (props) => {
       </div>
     );
   }
-
   // If the target is not set, it is better to have no skip navigation link
   // than a broken one, so we'll just output an empty span.
   return (<span></span>);
@@ -55,4 +37,4 @@ SkipNavigation.defaultProps = {
   a11yLink: '//www.nypl.org/accessibility',
 };
 
-export default Radium(SkipNavigation);
+export default SkipNavigation;
